a045009a0c1ceb0a190846a410aa31e92857469c
nikkoloz <nkhuk2019@agruni.edu.ge>
2022-11-13 01:28:50 +0400

    sd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="gg">
+      <button className="bg-red-500 text-white font-sm font-medium py-2 border-none rounded-md inline-block mb-4 mt-12 w-full">
+        ჩანაწერის დამატება
+      </button>
+    </section>
   );
 }
 
